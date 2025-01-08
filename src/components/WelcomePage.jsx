72d181af1fd62f8f5a70f61edbf32b5019f01c88
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-01-08 21:38:07 +0300

    initial commit

---
+++ src/components/WelcomePage.jsx
@@ -0,0 +1,8 @@
+const WelcomePage =()=>{
+    return(
+        <div>
+            <img src={"https://i.pinimg.com/236x/73/a7/28/73a72898a5265702b913ff227b8a0204.jpg"}/>
+        </div>
+    )
+}
+export default WelcomePage
